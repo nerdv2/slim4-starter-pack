4ec111215dfd043e82e8b444a033fb0be0d48098
Gema Aji Wardian <gema_wardian@hotmail.com>
2024-01-14 00:04:08 +0700

    Move default timezone to env and code cleanup

--- src/App/DotEnv.php
+++ src/App/DotEnv.php
@@ -7,4 +7,7 @@ $baseDir = __DIR__ . '/../../';
 $dotenv = Dotenv\Dotenv::createImmutable($baseDir);
 if (file_exists($baseDir . '.env')) {
     $dotenv->load();
+    if (!empty($_SERVER['DEFAULT_TIMEZONE'])) {
+        date_default_timezone_set($_SERVER['DEFAULT_TIMEZONE']);
+    }
 }
