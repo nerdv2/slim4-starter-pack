7e61ebc64045c3a4300876396dcb2cd1cddb831e
Gema Aji Wardian <gema_wardian@hotmail.com>
2025-02-07 17:09:24 +0700

    Allow cors setup on localhost only by default.

--- src/App/App.php
+++ src/App/App.php
@@ -7,7 +7,9 @@ require __DIR__ . '/DotEnv.php';
 $app = require __DIR__ . '/Container.php';
 $customErrorHandler = require __DIR__ . '/ErrorHandler.php';
 (require __DIR__ . '/Middlewares.php')($app, $customErrorHandler);
-(require __DIR__ . '/Cors.php')($app);
+if(in_array($_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1', 'localhost'))){
+    (require __DIR__ . '/Cors.php')($app);
+}
 (require __DIR__ . '/Database.php');
 (require __DIR__ . '/Routes.php');
 (require __DIR__ . '/NotFound.php')($app);
