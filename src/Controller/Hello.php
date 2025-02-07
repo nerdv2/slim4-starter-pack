f2a700abc0ae2227b7ce70d7b8fc3cf914837003
Gema Aji Wardian <gema_wardian@hotmail.com>
2025-02-07 17:14:21 +0700

    Add generate openAPI docs examples.

--- src/Controller/Hello.php
+++ src/Controller/Hello.php
@@ -11,10 +11,18 @@ use App\Model\HelloModel;
 
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use OpenApi\Attributes as OA;
 
+#[OA\Info(
+    title: "Slim4 Starter Pack", 
+    version: "v1.0.0", 
+    description: "This is backend API for Slim 4 Starter Pack, please use responsibly.",
+    contact: new OA\Contact(email: "admin@example.com")
+)]
 final class Hello
 {
     private Container $container;
+    private $hellomodel;
 
     public function __construct(Container $container)
     {
@@ -28,6 +36,8 @@ final class Hello
         return TwigResponse::render($request, $response, "hello.twig", $data, 200);
     }
 
+    #[OA\Get(path: '/status', tags: ["Default"], description: 'Retrieves application status and active version.', summary: "Default route")]
+    #[OA\Response(response: '200', description: "Success")]
     public function getStatusAPI(Request $request, Response $response): Response
     {
         $this->hellomodel = new HelloModel();
