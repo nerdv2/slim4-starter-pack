e15b75be14a96e924662ba45b77e5ff846aeb158
Gema Aji Wardian <gema_wardian@hotmail.com>
2026-09-15 15:30:16 +0700

    chore(routes): name the example routes

--- src/App/Routes.php
+++ src/App/Routes.php
@@ -4,13 +4,13 @@ declare(strict_types=1);
 
 // Start Route
 $app->get('/', 'App\Controller\Hello:getStatus')->setName('main');
-$app->get('/hello', 'App\Controller\Hello:getStatusAPI')->setName('main_api');
+$app->get('/hello', 'App\Controller\Hello:getStatusAPI')->setName('api.status');
 
 // Swagger Route
-$app->get('/swaggerui', 'App\Controller\Hello:openSwaggerUI');
+$app->get('/swaggerui', 'App\Controller\Hello:openSwaggerUI')->setName('swagger_ui');
 
 // Customer API Route
-$app->get('/customer', 'App\Controller\Customer:get');
-$app->post('/customer/add', 'App\Controller\Customer:add');
-$app->post('/customer/update', 'App\Controller\Customer:update');
-$app->delete('/customer/delete', 'App\Controller\Customer:delete');
+$app->get('/customer', 'App\Controller\Customer:get')->setName('api.customer.list');
+$app->post('/customer/add', 'App\Controller\Customer:add')->setName('api.customer.add');
+$app->post('/customer/update', 'App\Controller\Customer:update')->setName('api.customer.update');
+$app->delete('/customer/delete', 'App\Controller\Customer:delete')->setName('api.customer.delete');
